ac3d98ef89877f811e74da19f8d2132d757a98a3
zakaz <adam.staron@vp.pl>
2023-09-17 20:47:43 +0200

    update computers

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,17 +10,17 @@ const Computers = ({ isMobile }) => {
       <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
-        angle={0.12}
         penumbra={1}
         intensity={1}
+        angle={0.12}
         castShadow
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
